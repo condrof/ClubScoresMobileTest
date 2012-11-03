9ceb8da707801cad837abecdb7fded8ba21c7c08
fergal condron <condrof@gmail.com>
2012-11-03 18:47:39 +0000

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Jqmobtest::Application.config.secret_token = '17fe532891b685b6ec377a6ecfd02f3dc4e6e63466e8cd1dd05b903427f9d3e26785f6920f2b4b39c0c6607454ff35ba891d9fbeb155d8313ae7954332c45d8c'
